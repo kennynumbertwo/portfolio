3e3382b1bae06ec780598accd42eabf797f29299
Kenny Tye <ktye1217@gmail.com>
2021-12-12 08:31:58 -0800

    added hover effects on footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,26 @@
 import { syncBuiltinESMExports } from 'module';
 import styles from '../styles/Footer.module.css';
+import DiscordIcon from './icons/Discord';
+import GithubIcon from './icons/Github';
+import LinkedInIcon from './icons/LinkedIn';
+import MailIcon from './icons/Mail';
 
 function Footer() {
   return (
-    <footer className={styles.wrapper}>
+    <footer className={styles.container}>
       <div className={styles.iconsWrapper}>
-        <i className={styles.iconGithub} />
-        <i className={styles.iconDiscord} />
-        <i className={styles.iconLinkedIn} />
-        <i className={styles.iconMail} />
+        <span className={styles.iconGithub}>
+          <GithubIcon />
+        </span>
+        <span className={styles.iconDiscord}>
+          <DiscordIcon />
+        </span>
+        <span className={styles.iconLinkedIn}>
+          <LinkedInIcon />
+        </span>
+        <span className={styles.iconMail}>
+          <MailIcon />
+        </span>
       </div>
       <p className={styles.text}>
         This site was designed and built by Kenny Tye
