622a8f673216ba76ce815158eb9b50b63de934d8
Kenny Tye <ktye1217@gmail.com>
2021-12-23 23:09:37 -0800

    fine tuned the controls for the orbs

--- components/LinkFooterBubble.tsx
+++ components/LinkFooterBubble.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from '../styles/LinkFooterBubble.module.css';
 import AddIcon from './icons/Add';
+import MinusIcon from './icons/Minus';
 import PauseIcon from './icons/Pause';
 import PlayIcon from './icons/Play';
 import QuestionMarkIcon from './icons/QuestionMark';
@@ -13,37 +14,65 @@ function LinkFooterBubble({
   handleStopClick,
 }) {
   const [isShowingControls, setIsShowingContols] = useState<boolean>(false);
+  const [animation, setAnimation] = useState<string>('');
 
   const handleShowControls = () => {
     setIsShowingContols(true);
+  };
+  const handleHideControls = () => {
+    setAnimation('animateOut 350ms ease-in-out');
+    setTimeout(() => {
+      setAnimation('');
+      setIsShowingContols(false);
+    }, 340);
   };
 
   return (
     <div className={styles.iconsWrapperOuter}>
       <div className={styles.iconsWrapperInner}>
         {isShowingControls ? (
-          <>
-            <button onClick={handleAddOrbClick}>
-              <span className={styles.iconAdd}>
+          <div className={styles.controller}>
+            <button
+              onClick={handleAddOrbClick}
+              style={{ animation: `${animation}` }}
+            >
+              <span className={styles.iconTrash}>
                 <AddIcon />
               </span>
             </button>
-            <button onClick={handleClearOrbClick}>
+            <button
+              onClick={handleClearOrbClick}
+              style={{ animation: `${animation}` }}
+            >
               <span className={styles.iconTrash}>
                 <TrashIcon />
               </span>
             </button>
-            <button onClick={handleStartClick}>
+            <button
+              onClick={handleStartClick}
+              style={{ animation: `${animation}` }}
+            >
               <span className={styles.iconPlay}>
                 <PlayIcon />
               </span>
             </button>
-            <button onClick={handleStopClick}>
+            <button
+              onClick={handleStopClick}
+              style={{ animation: `${animation}` }}
+            >
               <span className={styles.iconPause}>
                 <PauseIcon />
               </span>
             </button>
-          </>
+            <button
+              onClick={handleHideControls}
+              style={{ animation: `${animation}` }}
+            >
+              <span className={styles.iconMinus}>
+                <MinusIcon />
+              </span>
+            </button>
+          </div>
         ) : (
           <>
             <button onClick={handleShowControls}>
