b990bd38c3ed2587a0225420ee9049eea7aa43fc
Kenny Tye <ktye1217@gmail.com>
2021-12-31 00:47:13 -0800

    fixing width on intro

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/Introduction.module.css';
 
 function Introduction() {
   return (
-    <section className={styles.container}>
+    <section className={styles.containerIntro}>
       <h4 className={styles.hello}>Hello, I'm</h4>
       <h1 className={styles.name}>Kenny Tye</h1>
       <h2 className={styles.developer}>I'm a web developer.</h2>
