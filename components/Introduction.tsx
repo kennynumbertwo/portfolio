476924f69111249572f9b909dab0d1af4e57329e
Kenny Tye <ktye1217@gmail.com>
2021-12-31 01:08:57 -0800

    working on styles for intro testing border 4

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,22 +1,20 @@
 import styles from '../styles/Introduction.module.css';
 
-function Introduction() {
+function Introduction(): JSX.Element {
   return (
-    <>
-      <div className={styles.containerIntro}>
-        <h4 className={styles.hello}>Hello, I'm</h4>
-        <h1 className={styles.name}>Kenny Tye</h1>
-        <h2 className={styles.developer}>I'm a web developer.</h2>
-        <div className={styles.descriptionWrapper}>
-          <p className={styles.description}>
-            I’m a developer specializing in building React applications. I’m a
-            natural tinkerer and love expanding my knowledge about coding and
-            technology. I hope to join a team of talented individuals that I can
-            learn from and build interesting projects with.
-          </p>
-        </div>
+    <div className={styles.containerIntro}>
+      <h4 className={styles.hello}>Hello, I'm</h4>
+      <h1 className={styles.name}>Kenny Tye</h1>
+      <h2 className={styles.developer}>I'm a web developer.</h2>
+      <div className={styles.descriptionWrapper}>
+        <p className={styles.description}>
+          I’m a developer specializing in building React applications. I’m a
+          natural tinkerer and love expanding my knowledge about coding and
+          technology. I hope to join a team of talented individuals that I can
+          learn from and build interesting projects with.
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
